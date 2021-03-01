43b8382d9a2ffaa4aaed627c8003f15256d93dfb
isabtan <77838546+isabtan@users.noreply.github.com>
2021-03-02 04:36:35 +0800

    try grid

--- p2/main.js
+++ p2/main.js
@@ -6,3 +6,21 @@ function openNav() {
 
   document.getElementById("start").style.opacity = "0";
 }
+
+var $win = $(window),
+    w = 0,
+    h = 0,
+    opacity = 0.3,
+    getWidth = function() {
+        w = $win.width();
+        h = $win.height();
+    };
+
+$win.resize(getWidth).mousemove(function(e) {
+
+    opacity = (e.pageX/w) * (e.pageY/h);
+    $('h1').text(opacity);
+
+    $('#bod').css('opacity',opacity);
+
+}).resize();
