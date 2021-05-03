2b408fe849b651c0bc208d55992a8c43d50194bc
isabtan <77838546+isabtan@users.noreply.github.com>
2021-05-03 23:50:50 +0800

    pls

--- p4/main.js
+++ p4/main.js
@@ -4,28 +4,33 @@ let elements = {
   'a': {
    'audio': 'rainfall.mp3',
    'video': 'img/rainfall.mp4',
-   name: "rainfall",
+    name: 'rainfall',
    // 'category': white nose,
  },
    'b': {
     'audio': 'slurping.mov',
     'video': 'img/slurping.mp4',
+    name: 'woman slurping ramen',
   },
   'c': {
    'audio': 'schoolbell.mp3',
    'video': 'img/schoolbell.mp4',
+   name: 'school bell ringing',
  },
    'd': {
     'audio': 'afghanistan.mp3',
     'video': 'img/afghanistan.mp4',
+    name: 'afghanistan national anthem',
   },
     'e': {
      'audio': 'fireplace.mov',
      'video': 'img/fireplace.mp4',
+     name: 'fireplace',
    },
    'f': {
     'audio': 'cooking.mov',
     'video': 'img/cooking.mp4',
+    name: 'frying eggs',
   },
     'g': {
      'audio': 'dogbarking.mp3',
@@ -51,14 +56,89 @@ let elements = {
      'audio': 'china.mp3',
      'video': 'img/china.mp4',
     },
+    'm': {
+     'audio': 'waterfall.mp3',
+     'video': 'img/waterfall.mp4',
+    },
+    'n': {
+     'audio': 'brushhair.mp4',
+     'video': 'img/brushhair-vid.mp4',
+    },
+    'o': {
+     'audio': 'alarm.mov',
+     'video': 'img/alarm.mp4',
+    },
+    'p': {
+     'audio': 'dominicanrep.mov',
+     'video': 'img/dominicanrep.mp4',
+    },
+    'q': {
+     'audio': 'boil.mp3',
+     'video': 'img/boil.mp4',
+    },
+    'r': {
+     'audio': 'typing.mp3',
+     'video': 'img/typing.mp4',
+    },
+    's': {
+     'audio': 'forkscrape.mp3',
+     'video': 'img/forkscrape.mp4',
+    },
+    't': {
+     'audio': 'egypt.mov',
+     'video': 'img/egypt.mp4',
+    },
+    'u': {
+     'audio': 'wind.wav',
+     'video': 'img/wind.mp4',
+    },
+    'v': {
+     'audio': 'poprocks.mp3',
+     'video': 'img/poprocks.mp4',
+    },
+    'w': {
+     'audio': 'spoonbite.mp3',
+     'video': 'img/spoonbite.mp4',
+    },
+    'x': {
+     'audio': 'fiji.mp3',
+     'video': 'img/fiji.mp4',
+    },
+    'y': {
+     'audio': 'treerustle.mov',
+     'video': 'img/treerustle.mp4',
+    },
+    'z': {
+     'audio': 'woodtap.m4a',
+     'video': 'img/woodtap.mp4',
+    },
 
 };
+
+//MARK
+let videoContainer = document.querySelector('.video-container');
+// this will loop through each `key` of your elements object e.g. a, b, c
+    //const just means you can reassign the variable, which you wouldn’t want to do in a loop anyway
+for(let key in elements) {
+  let vid = document.createElement('video');
+  vid.loop = true;
+  vid.autoplay = true;
+  vid.muted = true;
+  // access info from your object by using the key to access the values object
+  vid.src = elements[key].video;
+  // maybe assign the key as a data attribute so that later you can
+  // filter your videos and find the video that represents each key (to add a class to expand it)
+  vid.dataset.key = key;
+  videoContainer.append(vid);
+}
 
 let input = document.querySelector('input');
 let audio = document.querySelector('audio');
 let video = document.querySelector('video');
-// let name = document.querySelector('.des');
+let name = document.querySelector('.category');
 // let category = document.querySelector('.category');
+
+
 
 
                     // document.addEventListener("mouseup", function(event){
@@ -98,7 +178,7 @@ let video = document.querySelector('video');
     // console.log(elements['video']);
 
             //JUST TO TEST, PROPERTY ACCESSOR?
-  document.querySelector(".videos").src = elements['video'];
+  // document.querySelector(".videos").src = elements['video'];
 
 
 
@@ -142,7 +222,7 @@ function keyEvent(event) {
     video.classList.add("active");
 
     document.querySelector(".category").classList.add("cactive");
-    document.querySelector(".category").innerText = elements['name'];
+    document.querySelector(".category").innerHTML = `<h3>elements.name</h3>`;
     // category = data.category;
     // category.innerTextr = category;
 
