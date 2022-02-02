3f3e4ea92df3324b081e08d45b2a0c8592e370dd
isabtan <77838546+isabtan@users.noreply.github.com>
2022-02-01 23:02:26 -0500

    updated html

---
+++ p5/main.js
@@ -0,0 +1,579 @@
+let elements = {
+  'a': {
+   'audio': 'rainfall.mp3',
+   'video': 'img/rainfall.mp4',
+    name: 'rainfall',
+    category: 'white noise',
+   // 'category': white nose,
+ },
+   'b': {
+    'audio': 'slurping.mov',
+    'video': 'img/slurping.mp4',
+    name: 'woman slurping ramen',
+    category: 'asmr',
+  },
+  'c': {
+   'audio': 'schoolbell.mp3',
+   'video': 'img/schoolbell.mp4',
+   name: 'school bell ringing',
+   category: 'unpleasant',
+
+ },
+   'd': {
+    'audio': 'afghanistan.mp3',
+    'video': 'img/afghanistan.mp4',
+    name: 'afghanistan',
+    category: 'national anthem',
+  },
+    'e': {
+     'audio': 'fireplace.mov',
+     'video': 'img/fireplace.mp4',
+     name: 'fireplace',
+     category: 'white noise',
+   },
+   'f': {
+    'audio': 'cooking.mov',
+    'video': 'img/cooking.mp4',
+    name: 'frying eggs',
+    category: 'asmr',
+  },
+    'g': {
+     'audio': 'dogbarking.mp3',
+     'video': 'img/dogbarking.mp4',
+     name: 'dogs barking',
+     category: 'unpleasant',
+   },
+   'h': {
+    'audio': 'brunei.mp3',
+    'video': 'img/brunei.mp4',
+    name: 'brunei',
+    category: 'national anthem',
+  },
+    'i': {
+     'audio': 'ocean.mp3',
+     'video': 'img/ocean.mp4',
+     name: 'ocean waves',
+     category: 'white noise',
+   },
+     'j': {
+      'audio': 'whispers.mp3',
+      'video': 'img/whispers.mp4',
+      name: 'tired whispers',
+      category: 'asmr',
+    },
+    'k': {
+     'audio': 'crowdscream.mp3',
+     'video': 'img/crowdscream.mp4',
+     name: 'loud crowd screaming',
+     category: 'unpleasant',
+    },
+    'l': {
+     'audio': 'china.mp3',
+     'video': 'img/china.mp4',
+     name: 'china',
+     category: 'national anthem',
+    },
+    'm': {
+     'audio': 'waterfall.mp3',
+     'video': 'img/waterfall.mp4',
+     name: 'waterfall',
+     category: 'white noise',
+    },
+    'n': {
+     'audio': 'brushhair.mp4',
+     'video': 'img/brushhair-vid.mp4',
+     name: 'brushing through long hair',
+     category: 'asmr',
+    },
+    'o': {
+     'audio': 'alarm.mov',
+     'video': 'img/alarm.mp4',
+     name: 'apple alarm',
+     category: 'unpleasant',
+    },
+    'p': {
+     'audio': 'dominicanrep.mov',
+     'video': 'img/dominicanrep.mp4',
+     name: 'dominican republic',
+     category: 'dominican republic',
+    },
+    'q': {
+     'audio': 'boil.mp3',
+     'video': 'img/boil.mp4',
+     name: 'boiling water',
+     category: 'white noise',
+    },
+    'r': {
+     'audio': 'typing.mp3',
+     'video': 'img/typing.mp4',
+     name: 'typing on mechanical keyboard',
+     category: 'asmr',
+    },
+    's': {
+     'audio': 'forkscrape.mp3',
+     'video': 'img/forkscrape.mp4',
+     name: 'fork scraping on plate',
+     category: 'unpleasant',
+    },
+    't': {
+     'audio': 'egypt.mov',
+     'video': 'img/egypt.mp4',
+     name: 'egypt',
+     category: 'national anthem',
+    },
+    'u': {
+     'audio': 'wind.wav',
+     'video': 'img/wind.mp4',
+     name: 'strong wind blowing',
+     category: 'white noise',
+    },
+    'v': {
+     'audio': 'poprocks.mp3',
+     'video': 'img/poprocks.mp4',
+     name: 'popping candy',
+     category: 'asmr',
+    },
+    'w': {
+     'audio': 'spoonbite.mp3',
+     'video': 'img/spoonbite.mp4',
+     name: 'person biting spoon',
+     category: 'unpleasant',
+    },
+    'x': {
+     'audio': 'fiji.mp3',
+     'video': 'img/fiji.mp4',
+     name: 'fiji',
+     category: 'national anthem',
+    },
+    'y': {
+     'audio': 'treerustle.mov',
+     'video': 'img/treerustle.mp4',
+     name: 'bamboo trees rustling',
+     category: 'white noise',
+    },
+    'z': {
+     'audio': 'woodtap.m4a',
+     'video': 'img/woodtap.mp4',
+     name: 'wood tapping',
+     category: 'asmr',
+    },
+    '1': {
+     'audio': 'burp.m4a',
+     'video': 'img/burp.mp4',
+     name: 'man burping loud',
+     category: 'unpleasant',
+    },
+    '2': {
+     'audio': 'ghana.mp3',
+     'video': 'img/ghana.mp4',
+     name: 'ghana',
+     category: 'national anthem',
+    },
+    '3': {
+     'audio': 'treeswind.mp3',
+     'video': 'img/treewind.mp4',
+     name: 'trees blowing in wind',
+     category: 'white noise',
+    },
+    '4': {
+     'audio': 'sandraking.mp3',
+     'video': 'img/sandraking.mp4',
+     name: 'zen sand raking',
+     category: 'asmr',
+    },
+    '5': {
+     'audio': 'siren.mp3',
+     'video': 'img/siren.mp4',
+     name: 'hongkong police siren',
+     category: 'unpleasant',
+    },
+    '6': {
+     'audio': 'honduras.mov',
+     'video': 'img/honduras.mp4',
+     name: 'honduras',
+     category: 'national anthem',
+    },
+    '7': {
+     'audio': 'singingbowl.wav',
+     'video': 'img/singingbowl.mp4',
+     name: 'tibetan singing bowl',
+     category: 'asmr',
+    },
+    '8': {
+     'audio': 'honeycomb.mov',
+     'video': 'img/honeycomb.mp4',
+     name: 'person chewing honeycomb',
+     category: 'asmr',
+    },
+    '9': {
+     'audio': 'stirmacaroni.wav',
+     'video': 'img/stirmacaroni.mp4',
+     name: 'stirring macaroni & cheese',
+     category: 'unpleasant',
+    },
+    '0': {
+     'audio': 'indonesia.mp3',
+     'video': 'img/indonesia.mp4',
+     name: 'indonesia',
+     category: 'national anthem',
+    },
+    '-': {
+     'audio': 'droplet.mp3',
+     'video': 'img/droplets.mp4',
+     name: 'water droplets',
+     category: 'white noise',
+    },
+    '=': {
+     'audio': 'vibration.mp3',
+     'video': 'img/vibration.mp4',
+     name: 'large vibrations',
+     category: 'white noise',
+    },
+    '[': {
+     'audio': 'dishdrop.mp3',
+     'video': 'img/dishdrop.mp4',
+     name: 'dropping utensils',
+     category: 'unpleasant',
+    },
+    ']': {
+     'audio': 'japan.mov',
+     'video': 'img/japan.mp4',
+     name: 'japan',
+     category: 'national anthem',
+    },
+    ';': {
+     'audio': 'wipefabric.mp3',
+     'video': 'img/wipfabric.mp4',
+     name: 'cleaning fabric couch',
+     category: 'asmr',
+    },
+    ',': {
+     'audio': 'babycry.mp3',
+     'video': 'img/babycry.mp4',
+     name: 'baby crying',
+     category: 'unpleasant',
+    },
+    '.': {
+     'audio': 'train.mp3',
+     'video': 'img/train.mp4',
+     name: 'choo choo train',
+     category: 'white noise',
+    },
+    '/': {
+     'audio': 'footsteps.wav',
+     'video': 'img/footsteps.mp4',
+     name: 'footsteps',
+     category: 'white noise',
+    },
+    'A': {
+     'audio': 'crickets.mp3',
+     'video': 'img/crickets.mp4',
+     name: 'crickets chirping',
+     category: 'white noise',
+    },
+    'B': {
+     'audio': 'mixing.mp3',
+     'video': 'img/mixing.mp4',
+     name: 'mixing paint',
+     category: 'white noise',
+    },
+    'C': {
+     'audio': 'lawnmower.mp3',
+     'video': 'img/lawnmower.mp4',
+     name: 'lawnmower cutitng grass',
+     category: 'unpleasant',
+    },
+    'D': {
+     'audio': 'russia.mp3',
+     'video': 'img/russia.mp4',
+     name: 'russia',
+     category: 'national anthem',
+    },
+    'E': {
+     'audio': 'catpur.mp3',
+     'video': 'img/catpur.mp4',
+     name: 'cat purring',
+     category: 'asmr',
+    },
+    'F': {
+     'audio': 'tat.mp3',
+     'video': 'img/tat.mp4',
+     name: 'tattoo machine',
+     category: 'unpleasant',
+    },
+    'G': {
+     'audio': 'madlib.mp3',
+     'video': 'img/madlib.mp4',
+     name: 'Two for 2 - For dilla • madlib',
+     category: 'song',
+    },
+    'H': {
+     'audio': 'senegal.mp3',
+     'video': 'img/senegal.mp4',
+     name: 'senagal',
+     category: 'national anthem',
+    },
+    'I': {
+     'audio': 'daft.mp3',
+     'video': 'img/daft.mp4',
+     name: 'instant crush • daft punk ft. julian casablancas',
+     category: 'song',
+    },
+    'J': {
+     'audio': 'leaves.m4a',
+     'video': 'img/leaves.mp4',
+     name: 'walking on leaves',
+     category: 'asmr',
+    },
+    'K': {
+     'audio': 'chew.mp3',
+     'video': 'img/chew.mp4',
+     name: 'obnoxious chewing',
+     category: 'unpleasant',
+    },
+    'L': {
+     'audio': 'thailand.mp3',
+     'video': 'img/thailand.mp4',
+     name: 'thailand',
+     category: 'national anthem',
+    },
+    'M': {
+     'audio': 'saosaosao.mp3',
+     'video': 'img/fanchun.mp4',
+     name: 'Ruk Keu Fun Bpai • sao sao sao',
+     category: 'song',
+    },
+    'N': {
+     'audio': 'knocksmeoffmyfeet.mp3',
+     'video': 'img/stevie.mp4',
+     name: 'Knocks me off my feet • stevie wonder',
+     category: 'song',
+    },
+    'O': {
+     'audio': 'laugh.mov',
+     'video': 'img/laugh.mp4',
+     name: 'man laughing loud',
+     category: 'unpleasant',
+    },
+    'P': {
+     'audio': 'chs.mp3',
+     'video': 'img/chs.mp4',
+     name: 'too much sunshine • chs',
+     category: 'song',
+    },
+    'Q': {
+     'audio': 'gd.mp3',
+     'video': 'img/gd.mp4',
+     name: 'CROOKED • g-dragon',
+     category: 'song',
+    },
+    'R': {
+     'audio': 'timemovesslow.mp3',
+     'video': 'img/timemovesslow.mp4',
+     name: 'time moves slow • BADBADNOTGOOD ft. Sam Herring',
+     category: 'song',
+    },
+    'S': {
+     'audio': 'byyourside.mp3',
+     'video': 'img/sade.mp4',
+     name: 'By your side (neptunes remix) • sade',
+     category: 'song',
+    },
+    'T': {
+     'audio': 'yemen.mp3',
+     'video': 'img/yemen.mp4',
+     name: 'yemen',
+     category: 'national anthem',
+    },
+    'U': {
+     'audio': 'whereisthelove.mp3',
+     'video': 'img/whereisthelove.mp4',
+     name: 'where is love • the equatics',
+     category: 'song',
+    },
+    'V': {
+     'audio': 'vietnam.mp3',
+     'video': 'img/vietnam.mp4',
+     name: 'vietnam',
+     category: 'national anthem',
+    },
+    'W': {
+     'audio': 'fayewong.mp3',
+     'video': 'img/fayewong.mp4',
+     name: 'dream person • faye wong',
+     category: 'song',
+    },
+    'X': {
+     'audio': 'anything.mp3',
+     'video': 'img/anything.mp4',
+     name: 'anything • budgie',
+     category: 'song',
+    },
+    'Y': {
+     'audio': 'sinceileftu.mp3',
+     'video': 'img/sinceileftu.mp4',
+     name: 'since i left you • the avalanches',
+     category: 'song',
+    },
+    'Z': {
+     'audio': 'zionsville.mp3',
+     'video': 'img/zionsville.mp4',
+     name: 'zionsville • khruangbin',
+     category: 'song',
+    },
+    '!': {
+     'audio': 'birds.mp3',
+     'video': 'img/birds.mp4',
+     name: 'birds calling',
+     category: 'white noise',
+    },
+    '@': {
+     'audio': 'snipfabric.mp3',
+     'video': 'img/snip.mp4',
+     name: 'cutting through denim',
+     category: 'asmr',
+    },
+    '#': {
+     'audio': 'sault.mp3',
+     'video': 'img/sault.mp4',
+     name: 'wildfires • sault',
+     category: 'song',
+    },
+    '$': {
+     'audio': 'monaco.mp3',
+     'video': 'img/monaco.mp4',
+     name: 'monaco',
+     category: 'national anthem',
+    },
+    '%': {
+     'audio': 'plane.mp3',
+     'video': 'img/plane.mp4',
+     name: 'airplane cabin internal ambience',
+     category: 'white noise',
+    },
+    '^': {
+     'audio': 'write.mp3',
+     'video': 'img/write.mp4',
+     name: 'writing on paper',
+     category: 'asmr',
+    },
+    '&': {
+     'audio': 'concert.mp3',
+     'video': 'img/concert.mp4',
+     name: 'crowd screaming at a concert',
+     category: 'unpleasant',
+    },
+    '*': {
+     'audio': 'newzealand.mp3',
+     'video': 'img/newzealand.mp4',
+     name: 'new zealand',
+     category: 'national anthem',
+    },
+    '(': {
+     'audio': 'thunder.mp3',
+     'video': 'img/thunder.mp4',
+     name: 'approaching thunder',
+     category: 'white noise',
+    },
+    ')': {
+     'audio': 'nailtapping.mp3',
+     'video': 'img/nailtapping.mp4',
+     name: 'nail tapping on book',
+     category: 'asmr',
+    },
+
+};
+
+console.log(elements[0]);
+
+//MARK:
+let videoContainer = document.querySelector('.video-container');
+// this will loop through each `key` of your elements object e.g. a, b, c
+    //const just means you can reassign the variable, which you wouldn’t want to do in a loop anyway
+for(let key in elements) {
+  let vid = document.createElement('video');
+  vid.loop = true;
+  vid.autoplay = true;
+  vid.muted = true;
+  // access info from your object by using the key to access the values object
+  vid.src = elements[key].video;
+  // maybe assign the key as a data attribute so that later you can
+  // filter your videos and find the video that represents each key (to add a class to expand it)
+  vid.dataset.key = key;
+    // elements[event.key].dataset.key = key;
+    // key.dataset.key = event.key;
+  videoContainer.append(vid);
+}
+
+let input = document.querySelector('input');
+let audio = document.querySelector('audio');
+// let video = document.querySelector('video');
+let videos = Array.from(document.querySelectorAll('video'));
+let nameDisplay = document.querySelector('.des');
+let pressDisplay = document.querySelector('.show-pressed');
+let catDisplay = document.querySelector('.category');
+// let category = document.querySelector('.category');
+
+//MARK, 2:
+function keyEvent(event) {
+  // see if the key typed is one of the keys in our `elements` object above
+  if (event.key in elements) {
+    audio.pause();
+        // video.play();
+        // video.classList.remove("active");
+  // first see if there's an active video, pause it and remove active class
+    let activeVideo = videos.find(v => v.classList.contains('active'));
+      if (activeVideo) {
+        activeVideo.pause();
+        activeVideo.classList.remove('active');
+      }
+
+      let video = videos.find(v => v.dataset.key == event.key);
+        video.classList.add('active');
+        video.play();
+
+    let data = elements[event.key];
+    audio.src = data.audio;
+    audio.play();
+
+    nameDisplay.innerText = data.name;
+    catDisplay.innerText = data.category;
+    pressDisplay.innerText = event.key;
+      // pressDisplay.classList.add("press");
+
+  }
+}
+document.addEventListener('keyup', keyEvent);
+
+let mobClick = window.matchMedia('(max-width: 1025px)');
+
+if (mobClick.matches) {
+  document.removeEventListener('keyup', keyEvent);
+	document.addEventListener('touchstart', clickForVids);
+
+  function clickForVids(){
+    console.log("click mobile pls")
+    audio.pause();
+
+    let activeVideo = videos.find(v => v.classList.contains('active'));
+      if (activeVideo) {
+        activeVideo.play();
+        audio.play();
+        activeVideo.classList.toggle('active');
+
+        nameDisplay.innerText = data.name;
+        catDisplay.innerText = data.category;
+      }
+
+
+    //   let video = videos.find(v => v.dataset.key == event.key);
+    //     video.classList.add('active-mob');
+    //     video.play();
+    //
+    // let data = elements[event.key];
+    // audio.src = data.audio;
+    // audio.play();
+
+
+  }
+}
